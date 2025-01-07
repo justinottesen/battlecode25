d1028027f9a3334702f47cdd194ecdb6aac337dd
Justin Ottesen <justinottesen@gmail.com>
2025-01-06 20:35:59 -0500

    Initial class configuration set up

---
+++ java/src/sprint_1/Mopper.java
@@ -0,0 +1,17 @@
+package sprint_1;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class Mopper extends Robot {
+
+  public Mopper(RobotController rc) {
+    super(rc);
+  }
+
+  @Override
+  public void run() throws GameActionException {
+
+  }
+
+};

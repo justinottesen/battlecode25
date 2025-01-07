42ee6444a65f94c97b92e8d6acc30c37c6940c05
Justin Ottesen <justinottesen@gmail.com>
2025-01-07 11:16:45 -0500

    Add explore code

--- java/src/sprint_1/robots/Mopper.java
+++ java/src/sprint_1/robots/Mopper.java
@@ -1,7 +1,12 @@
 package sprint_1.robots;
 
+import java.util.Random;
+
+import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import battlecode.common.UnitType;
 
 public class Mopper extends Robot {
 
@@ -9,9 +14,29 @@ public class Mopper extends Robot {
     super(rc);
   }
 
+  static final Random rng = new Random(6147);
+  static boolean spawnOne = true;
+  /** Array containing all the possible movement directions. */
+  static final Direction[] directions = {
+      Direction.NORTH,
+      Direction.NORTHEAST,
+      Direction.EAST,
+      Direction.SOUTHEAST,
+      Direction.SOUTH,
+      Direction.SOUTHWEST,
+      Direction.WEST,
+      Direction.NORTHWEST,
+  };
+
   @Override
   public void run() throws GameActionException {
-
+    // Pick a direction to build in.
+    Direction dir = directions[rng.nextInt(directions.length)];
+    MapLocation nextLoc = rc.getLocation().add(dir);
+    if (rc.canBuildRobot(UnitType.SOLDIER, nextLoc) && spawnOne){
+      rc.buildRobot(UnitType.SOLDIER, nextLoc);
+      spawnOne=false;
+    }
   }
 
 };

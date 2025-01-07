96c52355ee253fc28154c9b08ddb50c07bda9ce8
abank <banka@rpi.edu>
2025-01-06 22:28:04 -0500

    testing code for bugnav

--- java/src/sprint_1/Soldier.java
+++ java/src/sprint_1/Soldier.java
@@ -1,15 +1,25 @@
 package sprint_1;
 
+import java.util.Random;
+
 import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
+import battlecode.common.MapLocation;
 
 public class Soldier extends Robot {
+  static final Random rng = new Random(6147);
+  MapLocation goal;
+  Pathfinding pathfinding;
+
   public Soldier(RobotController rc) {
     super(rc);
+    goal = new MapLocation(rng.nextInt(rc.getMapWidth()),rng.nextInt(rc.getMapHeight()));
+    pathfinding=new Pathfinding();
   }
 
   @Override
   public void run() throws GameActionException {
-
+    pathfinding.moveTo(goal);
+    rc.setIndicatorString("Moving to "+goal);
   }
 }

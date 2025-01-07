b444b988c7871b3a73018841600d742efc6ce268
Andrew Bank <122238344+andrewkbank@users.noreply.github.com>
2025-01-06 22:29:22 -0500

    Bugpath (#5)
    * Copied XSquare Bugpath
    
    * testing code for bugnav

--- java/src/sprint_1/Tower.java
+++ java/src/sprint_1/Tower.java
@@ -1,17 +1,42 @@
 package sprint_1;
 
+import java.util.Random;
+
+import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import battlecode.common.UnitType;
 
 public class Tower extends Robot {
 
+
+  static final Random rng = new Random(6147);
+  static boolean spawnOne = true;
+    /** Array containing all the possible movement directions. */
+    static final Direction[] directions = {
+        Direction.NORTH,
+        Direction.NORTHEAST,
+        Direction.EAST,
+        Direction.SOUTHEAST,
+        Direction.SOUTH,
+        Direction.SOUTHWEST,
+        Direction.WEST,
+        Direction.NORTHWEST,
+    };
   public Tower(RobotController rc) {
     super(rc);
   }
 
   @Override
   public void run() throws GameActionException {
-
+    // Pick a direction to build in.
+        Direction dir = directions[rng.nextInt(directions.length)];
+        MapLocation nextLoc = rc.getLocation().add(dir);
+        if (rc.canBuildRobot(UnitType.SOLDIER, nextLoc) && spawnOne){
+            rc.buildRobot(UnitType.SOLDIER, nextLoc);
+            spawnOne=false;
+        }
   }
 
 };

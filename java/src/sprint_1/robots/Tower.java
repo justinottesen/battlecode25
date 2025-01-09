041de5cad3fdba70b7aec39e04d1edef94227783
Matthew Voynovich <143434817+mvoynovich@users.noreply.github.com>
2025-01-09 15:04:27 -0500

    Tower logic (#24)
    * Tower attack logic
    
    * Tower attack logic
    
    * Update Tower.java
    
    * Fixed attack logic and added some spawning and saving
    
    Uses both attack types now
    
    After a certain round we do not spawn without keeping enough money to rebuild a tower for paint
    
    * Fix

--- java/src/sprint_1/robots/Tower.java
+++ java/src/sprint_1/robots/Tower.java
@@ -6,6 +6,7 @@ import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
 import battlecode.common.UnitType;
 
 public class Tower extends Robot {
@@ -14,6 +15,9 @@ public class Tower extends Robot {
   static final Random rng = new Random(6147);
   static boolean spawnOne = true;
   static boolean spawnMopper = true;
+
+
+
     /** Array containing all the possible movement directions. */
     static final Direction[] directions = {
         Direction.NORTH,
@@ -27,24 +31,63 @@ public class Tower extends Robot {
     };
   public Tower(RobotController rc) {
     super(rc);
+
   }
 
   @Override
   public void run() throws GameActionException {
-    // Pick a direction to build in.
-    Direction dir = directions[rng.nextInt(directions.length)];
-    MapLocation nextLoc = rc.getLocation().add(dir);
-    if (rc.canBuildRobot(UnitType.SOLDIER, nextLoc) && spawnOne){
-        rc.buildRobot(UnitType.SOLDIER, nextLoc);
-        spawnOne=false;
+    Direction dir;
+    MapLocation nextLoc;
+    if (spawnOne){
+      dir = directions[rng.nextInt(directions.length)];
+      nextLoc = rc.getLocation().add(dir);
+      if (rc.canBuildRobot(UnitType.SOLDIER, nextLoc)){
+          rc.buildRobot(UnitType.SOLDIER, nextLoc);
+          spawnOne = false;
+      }
     }
 
-    dir = directions[rng.nextInt(directions.length)];
-    nextLoc = rc.getLocation().add(dir);
-    if (rc.canBuildRobot(UnitType.MOPPER, nextLoc) && spawnMopper) {
-      rc.buildRobot(UnitType.MOPPER, nextLoc);
-      spawnMopper = false;
+    if (spawnMopper){
+      dir = directions[rng.nextInt(directions.length)];
+      nextLoc = rc.getLocation().add(dir);
+      if (rc.canBuildRobot(UnitType.MOPPER, nextLoc)) {
+        rc.buildRobot(UnitType.MOPPER, nextLoc);
+        spawnMopper = false;
+      }
+    }
+
+
+    // Pick a direction to build in.
+
+
+
+    // If we are losing money as fast or faster than last turn keep spending
+    // If we are gaining money, we are saving so don't produce
+    if(rc.getRoundNum() > 500 && rc.getMoney() > 1250){
+      dir = directions[rng.nextInt(directions.length)];
+      nextLoc = rc.getLocation().add(dir);
+      if (rc.canBuildRobot(UnitType.SOLDIER, nextLoc)){
+        rc.buildRobot(UnitType.SOLDIER, nextLoc);
+      }
+    }
+
+
+    // Only programmed in towers without defense boost as current gameplan does not involve defense
+    RobotInfo[] enemyRobots = rc.senseNearbyRobots(rc.getType().actionRadiusSquared , rc.getTeam().opponent());
+    
+    if (enemyRobots.length > 0){
+      RobotInfo lowestHealth = enemyRobots[0];
+      rc.attack(null);
+    // Check if there is an enemy you can one shot 
+      for (RobotInfo enemy : enemyRobots){
+        if (enemy.getHealth() < lowestHealth.getHealth() && enemy.getHealth() >= rc.getType().attackStrength){
+          lowestHealth = enemy;
+        }
+      }
+      rc.attack(lowestHealth.location);
     }
   }
+
+
 
 };

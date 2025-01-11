a94a283f181b80fb52e3b121410e920fd16543cf
Justin Ottesen <justinottesen@gmail.com>
2025-01-10 19:41:47 -0500

    Added tower round 1 spawning logic

--- java/src/jottesen_test/Robot.java
+++ java/src/jottesen_test/Robot.java
@@ -8,11 +8,20 @@ public abstract class Robot {
   
   protected final Team TEAM;
   protected final Team OPPONENT;
+
+  protected final int MAP_WIDTH;
+  protected final int MAP_HEIGHT;
+  protected final MapLocation MAP_CENTER;
   
   public Robot(RobotController rc_) {
     rc = rc_;
+
     TEAM = rc.getTeam();
     OPPONENT = TEAM.opponent();
+
+    MAP_WIDTH = rc.getMapWidth();
+    MAP_HEIGHT = rc.getMapHeight();
+    MAP_CENTER = new MapLocation(MAP_WIDTH / 2, MAP_HEIGHT / 2);
   };
 
   public abstract void run() throws GameActionException;

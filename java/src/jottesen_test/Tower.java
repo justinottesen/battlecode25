c4bfa6b6836b06dbe0705038ff0c082fd24b3ef0
Justin Ottesen <justinottesen@gmail.com>
2025-01-10 19:41:47 -0500

    Added tower round 1 spawning logic

--- java/src/jottesen_test/Tower.java
+++ java/src/jottesen_test/Tower.java
@@ -6,15 +6,22 @@ public class Tower extends Robot {
 
   protected final int TOWER_ATTACK_RADIUS;
 
+  protected final MapLocation LOCATION;
+
   public Tower(RobotController rc_) {
     super(rc_);
 
     TOWER_ATTACK_RADIUS = rc.getType().actionRadiusSquared; // NOTE: This will have to change if upgrades modify radius
+
+    LOCATION = rc.getLocation();
   }
 
   public void run() throws GameActionException {
     // Attack enemies
     attackEnemies();
+    
+    // Spawn Robots
+    spawnRobots();
   }
 
   /**
@@ -47,6 +54,62 @@ public class Tower extends Robot {
     // Attack enemy
     if (rc.canAttack(target.getLocation())) {
       rc.attack(target.getLocation());
+    }
+  }
+
+  /**
+   * Handles the logic of spawning robots.
+   * 
+   * Each tower simply spawns a single soldier at the start of the game.
+   * The location of the spawned soldier is in the closest available direction
+   * to the middle of the map.
+   * 
+   * @throws GameActionException
+   */
+  private void spawnRobots() throws GameActionException {
+    // Early Game Special Cases
+    if (rc.getRoundNum() == 1) {
+      Direction r_dir = LOCATION.directionTo(MAP_CENTER);
+      if (rc.canBuildRobot(UnitType.SOLDIER, LOCATION.add(r_dir))) {
+        rc.buildRobot(UnitType.SOLDIER, LOCATION.add(r_dir));
+        return;
+      }
+      Direction l_dir = r_dir.rotateLeft();
+      if (rc.canBuildRobot(UnitType.SOLDIER, LOCATION.add(l_dir))) {
+        rc.buildRobot(UnitType.SOLDIER, LOCATION.add(l_dir));
+        return;
+      }
+      r_dir = r_dir.rotateRight();
+      if (rc.canBuildRobot(UnitType.SOLDIER, LOCATION.add(r_dir))) {
+        rc.buildRobot(UnitType.SOLDIER, LOCATION.add(r_dir));
+        return;
+      }
+      l_dir = l_dir.rotateLeft();
+      if (rc.canBuildRobot(UnitType.SOLDIER, LOCATION.add(l_dir))) {
+        rc.buildRobot(UnitType.SOLDIER, LOCATION.add(l_dir));
+        return;
+      }
+      r_dir = r_dir.rotateRight();
+      if (rc.canBuildRobot(UnitType.SOLDIER, LOCATION.add(r_dir))) {
+        rc.buildRobot(UnitType.SOLDIER, LOCATION.add(r_dir));
+        return;
+      }
+      l_dir = l_dir.rotateLeft();
+      if (rc.canBuildRobot(UnitType.SOLDIER, LOCATION.add(l_dir))) {
+        rc.buildRobot(UnitType.SOLDIER, LOCATION.add(l_dir));
+        return;
+      }
+      r_dir = r_dir.rotateRight();
+      if (rc.canBuildRobot(UnitType.SOLDIER, LOCATION.add(r_dir))) {
+        rc.buildRobot(UnitType.SOLDIER, LOCATION.add(r_dir));
+        return;
+      }
+      l_dir = l_dir.rotateLeft();
+      if (rc.canBuildRobot(UnitType.SOLDIER, LOCATION.add(l_dir))) {
+        rc.buildRobot(UnitType.SOLDIER, LOCATION.add(l_dir));
+        return;
+      }
+      return;
     }
   }
 }

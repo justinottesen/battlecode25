20b5a494c88a68744609f735b8ece5650b2c2806
Justin Ottesen <justinottesen@gmail.com>
2025-01-10 19:19:39 -0500

    Towers attack enemy units

--- java/src/jottesen_test/Tower.java
+++ java/src/jottesen_test/Tower.java
@@ -4,9 +4,49 @@ import battlecode.common.*;
 
 public class Tower extends Robot {
 
-  public Tower(RobotController rc) {
-    super(rc);
+  protected final int TOWER_ATTACK_RADIUS;
+
+  public Tower(RobotController rc_) {
+    super(rc_);
+
+    TOWER_ATTACK_RADIUS = rc.getType().actionRadiusSquared; // NOTE: This will have to change if upgrades modify radius
   }
 
-  public void run() {}
+  public void run() throws GameActionException {
+    // Attack enemies
+    attackEnemies();
+  }
+
+  /**
+   * Attacks all enemies with AoE, and targets the first enemy found which can
+   * be killed in one shot. Otherwise, targets the lowest health enemy.
+   * 
+   * @throws GameActionException
+   */
+  private void attackEnemies() throws GameActionException {
+    // Find nearby enemies
+    RobotInfo[] enemies = rc.senseNearbyRobots(TOWER_ATTACK_RADIUS, OPPONENT);
+    if (enemies.length == 0) { return; }
+    
+    // Perform AoE attack
+    rc.attack(null);
+
+    // Find weakest or one-shot enemy
+    RobotInfo target = enemies[0];
+    if (target.getHealth() > rc.getType().attackStrength) {
+      for (RobotInfo enemy : enemies) {
+        if (enemy.getHealth() < target.getHealth()) {
+          target = enemy;
+          if (target.getHealth() < rc.getType().attackStrength) {
+            break;
+          }
+        }
+      }
+    }
+
+    // Attack enemy
+    if (rc.canAttack(target.getLocation())) {
+      rc.attack(target.getLocation());
+    }
+  }
 }

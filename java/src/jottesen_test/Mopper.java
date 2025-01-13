bac717278eb9293305f2e14c67a985714a32437c
Justin Ottesen <justinottesen@gmail.com>
2025-01-13 17:37:38 -0500

    Improved mopper micro

--- java/src/jottesen_test/Mopper.java
+++ java/src/jottesen_test/Mopper.java
@@ -34,6 +34,32 @@ public final class Mopper extends Robot {
     // Can't do anything, no point
     if (!rc.isMovementReady() && !rc.isActionReady()) { return; }
 
+    // Look for nearby enemy paint 
+    // TODO: Prioritize enemies
+    if (rc.isActionReady()) {
+      for (MapInfo info : rc.senseNearbyMapInfos(rc.getType().actionRadiusSquared)) {
+        if (info.getPaint().isEnemy() && rc.canAttack(info.getMapLocation())) {
+          rc.attack(info.getMapLocation());
+          break;
+        }
+      }
+    }
+
+    // Transfer paint if possible
+    if (rc.isActionReady() && rc.getPaint() > GameConstants.INCREASED_COOLDOWN_THRESHOLD) {
+      for (RobotInfo robot : rc.senseNearbyRobots(GameConstants.PAINT_TRANSFER_RADIUS_SQUARED)) {
+        // Don't transfer to other moppers or towers, or if they have enough paint
+        if (robot.type == UnitType.MOPPER || robot.getType().isTowerType() || robot.getPaintAmount() > GameConstants.INCREASED_COOLDOWN_THRESHOLD) { continue; }
+
+        // Transfer the max possible
+        int transfer_amt = Math.min(robot.getType().paintCapacity - robot.getPaintAmount(), rc.getPaint() - GameConstants.INCREASED_COOLDOWN_THRESHOLD);
+        if (rc.canTransferPaint(robot.getLocation(), transfer_amt)) {
+          rc.transferPaint(robot.getLocation(), transfer_amt);
+          break;
+        }
+      }
+    }
+
     // If we find a tower while refilling paint, refill and set back idle
     if (goal == REFILL_PAINT) {
       if (rc.getLocation().isWithinDistanceSquared(pathfinding.getTarget(), GameConstants.PAINT_TRANSFER_RADIUS_SQUARED)) {
@@ -50,30 +76,39 @@ public final class Mopper extends Robot {
           pathfinding.setTarget(mapData.MAP_CENTER);
         }
       }
-    }
-
-    // Look for nearby enemy paint 
-    // TODO: Prioritize enemies
-    if (rc.isActionReady()) {
-      for (MapInfo info : rc.senseNearbyMapInfos(rc.getType().actionRadiusSquared)) {
-        if (info.getPaint().isEnemy() && rc.canAttack(info.getMapLocation())) {
-          rc.attack(info.getMapLocation());
-          break;
+    } else {
+      // Look for enemy paint to move towards (and attack potentially)
+      if (rc.isMovementReady()) {
+        for (MapInfo info : rc.senseNearbyMapInfos(GameConstants.VISION_RADIUS_SQUARED)) {
+          if (info.getPaint().isEnemy() && !rc.getLocation().isWithinDistanceSquared(info.getMapLocation(), rc.getType().actionRadiusSquared)) {
+            Direction dir = pathfinding.getGreedyMove(rc.getLocation(), info.getMapLocation(), true, true);
+            if (dir != null) {
+              mapData.move(dir);
+              if (rc.canAttack(info.getMapLocation())) {
+                rc.attack(info.getMapLocation());
+              }
+              break;
+            }
+          }
         }
       }
-    }
 
-    // Transfer paint if possible
-    if (rc.isActionReady() && rc.getPaint() > GameConstants.INCREASED_COOLDOWN_THRESHOLD / 2) {
-      for (RobotInfo robot : rc.senseNearbyRobots(GameConstants.PAINT_TRANSFER_RADIUS_SQUARED)) {
-        // Don't transfer to other moppers or towers
-        if (robot.type == UnitType.MOPPER || robot.getType().isTowerType()) { continue; }
-
-        // Transfer the max possible
-        int transfer_amt = Math.min(robot.getType().paintCapacity - robot.getPaintAmount(), rc.getPaint()- GameConstants.INCREASED_COOLDOWN_THRESHOLD / 2);
-        if (rc.canTransferPaint(robot.getLocation(), transfer_amt)) {
-          rc.transferPaint(robot.getLocation(), transfer_amt);
-          break;
+      // Look for friendly soldiers to move towards
+      if (rc.isMovementReady()) {
+        for (RobotInfo info : rc.senseNearbyRobots(-1, TEAM)) {
+          if (info.type == UnitType.MOPPER || info.getType().isTowerType() || info.getPaintAmount() > GameConstants.INCREASED_COOLDOWN_THRESHOLD) { continue; }
+          if (rc.getLocation().distanceSquaredTo(info.getLocation()) > GameConstants.PAINT_TRANSFER_RADIUS_SQUARED) {
+            Direction dir = pathfinding.getGreedyMove(rc.getLocation(), info.getLocation(), true, true);
+            if (dir != null) {
+              mapData.move(dir);
+              // Transfer the max possible
+              int transfer_amt = Math.min(info.getType().paintCapacity - info.getPaintAmount(), rc.getPaint() - GameConstants.INCREASED_COOLDOWN_THRESHOLD);
+              if (rc.canTransferPaint(info.getLocation(), transfer_amt)) {
+                rc.transferPaint(info.getLocation(), transfer_amt);
+                break;
+              }
+            }
+          }
         }
       }
     }

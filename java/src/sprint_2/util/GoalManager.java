eb8b0320651e126662905e9baad36baab415acce
Justin Ottesen <justinottesen@gmail.com>
2025-02-03 19:21:31 -0500

    Add back in sprint 2

---
+++ java/src/sprint_2/util/GoalManager.java
@@ -0,0 +1,114 @@
+package sprint_2.util;
+
+import battlecode.common.*;
+
+/**
+ * THIS CLASS WILL NEVER LET THE GOAL STACK BE EMPTY.
+ * IF NO GOAL IS SPECIFIED, IT WILL DEFAULT TO EXPLORE
+ */
+public class GoalManager {
+  
+  private static final int GOAL_STACK_SIZE = 10;
+  private static final Stack<Goal> goalStack = new Stack<Goal>(GOAL_STACK_SIZE);
+
+  public static void init() {
+    goalStack.push(new Goal(Goal.Type.EXPLORE, MapData.getExploreTarget()));
+    Pathfinding.setTarget(goalStack.top());
+  }
+
+  /**
+   * Gets the current goal
+   * @return The current goal
+   */
+  public static Goal current() { return goalStack.top(); }
+
+  /**
+   * Pushes a goal to the stack
+   * @param type The type of the goal to push
+   * @param target The target location of the goal
+   */
+  public static boolean pushGoal(Goal.Type type, MapLocation target) { return pushGoal(new Goal(type, target)); }
+
+  /**
+   * Pushes a goal to the stack
+   * @param goal The goal to push to the stack
+   */
+  public static boolean pushGoal(Goal goal) {
+    if (goalStack.push(goal)) {
+      Pathfinding.setTarget(goal);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Pops a goal off the stack, setting the next goal as the active goal, or
+   * if no other goal is present, the default goal of explore
+   */
+  public static void popGoal() { popGoal(new Goal(Goal.Type.EXPLORE, MapData.getExploreTarget())); }
+
+  /**
+   * Pops a goal off the stack, setting the next goal as the 
+   * active goal, or `fallback` if there isn't one
+   * @param fallback The goal to default to if there is no secondary goal
+   */
+  public static void popGoal(Goal fallback) {
+    goalStack.pop();
+    if (goalStack.empty()) { goalStack.push(fallback); }
+    Pathfinding.setTarget(goalStack.top());
+  }
+
+  /**
+   * Clears the stack and sets a new goal
+   * @param type The type of the new goal to set
+   * @param target The location to set as the target for the goal
+   */
+  public static void setNewGoal(Goal.Type type, MapLocation target) { setNewGoal(new Goal(type, target)); }
+
+  /**
+   * Clears the stack and sets a new goal
+   * @param goal The new goal to set
+   */
+  public static void setNewGoal(Goal goal) {
+    goalStack.clear();
+    goalStack.push(goal);
+    Pathfinding.setTarget(goal);
+  }
+
+  /**
+   * Replaces the current goal with the specified goal, preserving the rest of the stack
+   * @param type The type of the new goal to set
+   * @param target The location to set as the target for the goal
+   */
+  public static void replaceTopGoal(Goal.Type type, MapLocation target) { replaceTopGoal(new Goal(type, target)); }
+
+  /**
+   * Replaces the current goal with the specified goal, preserving the rest of the stack
+   * @param goal The new goal to set
+   */
+  public static void replaceTopGoal(Goal goal) {
+    goalStack.pop();
+    goalStack.push(goal);
+    Pathfinding.setTarget(goal);
+  }
+
+  /**
+   * Returns whether goal is in the stack
+   * @param type The type of the new goal to set
+   * @param target The location to set as the target for the goal
+   */
+  public static boolean contains(Goal.Type type, MapLocation target){
+    Goal g = new Goal(type,target);
+    return goalStack.contains(g);
+  }
+
+  /**
+   * Pushes goal to the bottom of the stack
+   * @param type The type of the new goal to set
+   * @param target The location to set as the target for the goal
+   */
+  public static boolean pushSecondaryGoal(Goal.Type type, MapLocation target){
+    if(goalStack.available()==0) return false;
+    return goalStack.insert(new Goal(type,target),0);
+  }
+}
---
+++ java/src/sprint_2/util/Stack.java
@@ -0,0 +1,138 @@
+package sprint_2.util;
+
+/**
+ * Simple fixed size stack helper class
+ */
+public class Stack<T> {
+  
+  private T[] data;
+  private int index = 0;
+  
+  public Stack(int max_size) {
+    data = (T[]) new Object[max_size];
+  }
+
+  /**
+   * Adds an item onto the stack.
+   * @param item The item to be added to the stack
+   * @return Whether the item was successfully added to the stack
+   */
+  public boolean push(T item) {
+    if (index == data.length) { return false; }
+    data[index++] = item;
+    return true;
+  }
+
+  /**
+   * Inserts an item to the given position on the stack.
+   * It replaces the current item at that index and bumps the rest
+   * of the stack up.
+   * @param item The item to insert
+   * @param i The index in the stack to insert the item
+   * @return Whether the item was successfully inserted
+   */
+  public boolean insert(T item, int i) {
+    if (index == data.length || i > index) { return false; }
+    for (int j = index++; j > i; --j) {
+      data[j] = data[j - 1];
+    }
+    data[i] = item;
+    return true;
+  }
+
+  /**
+   * Removes an item from the stack.
+   * @return The item which was removed, or `null` if empty
+   */
+  public T pop() {
+    return (index == 0 ? null : data[--index]);
+  }
+
+  /**
+   * Removes the item at the given position from the stack.
+   * @return The item that was removed or null
+   */
+  public T remove(int i) {
+    if (index == 0 || i >= index) { return null; }
+    T ret = data[i];
+    --index;
+    for (int j = i; i < index; ++i) {
+      data[j] = data[j+1];
+    }
+    return ret; 
+  }
+
+  /**
+   * Peeks at the next item on the stack without modifying it
+   * @return The top item on the stack
+   */
+  public T top() {
+    return (index == 0 ? null : data[index - 1]);
+  }
+
+  /**
+   * Returns the item at the given index
+   * @param i The index to retrieve the entry at
+   */
+  public T peek(int i) {
+    return (i < index ? data[i] : null);
+  }
+
+  /**
+   * Peeks at the second item on the stack without modifying it
+   * @return The second item on the stack
+   */
+  public T peekSecond() {
+    return (index <= 1 ? null : data[index - 2]);
+  }
+
+  /**
+   * Returns the item at the bottom of the stack without modification.
+   * This is not a conventional stack method.
+   * @return The first element in the array (bottom of the stack)
+   */
+  public T bottom() {
+    return (index == 0 ? null : data[0]);
+  }
+
+  /**
+   * Returns the total capacity of the stack
+   * @return The size of the data array
+   */
+  public int capacity() { return data.length; }
+
+  /**
+   * Returns the usage of the stack
+   * @return The number of elements filled in the array
+   */
+  public int used() { return index; }
+
+  /**
+   * Returns the available space in the stack
+   * @return The number of unused spaces in the array
+   */
+  public int available() { return data.length - index; }
+
+  /**
+   * Returns whether the stack is empty
+   * @return Whether the stack is empty
+   */
+  public boolean empty() { return index == 0; }
+
+  /**
+   * Removes all elements from the stack
+   */
+  public void clear() { index = 0; }
+
+  /**
+   * Checks whether a given item is in the stack
+   */
+  public boolean contains(T obj) {
+    for (int i = 0; i < index; ++i) {
+      if (data[i].equals(obj)) { return true; }
+    }
+    return false;
+  }
+
+}
+
